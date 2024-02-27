e52db012fe2813dd7a9c844817ee18340f13e0f9
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-02-27 01:44:02 -0500

    Train ready.

--- get_dataloader.py
+++ get_dataloader.py
@@ -12,8 +12,10 @@ for lang in c2t.values():
     tokenizers[lang] = make_tokenizer(lang)
 
 class MonoDataset(Dataset):
-    def __init__(self, file):
+    def __init__(self, file, num_examples):
         self.examples = [line.strip() for line in open(file, 'r', encoding='utf-8').readlines()]
+        if num_examples:
+            self.examples = self.examples[:num_examples]
         self.lang_code = os.path.basename(file).split('.')[0].split('_')[0]
         self.lang_token = c2t[self.lang_code]
         self.tokenizer = tokenizers[self.lang_token]
@@ -25,8 +27,10 @@ class MonoDataset(Dataset):
         return len(self.examples)
     
 class ParallelDataset(Dataset):
-    def __init__(self, file):
+    def __init__(self, file, num_examples):
         self.examples = [line.strip().split('\t') for line in open(file, 'r', encoding='utf-8').readlines()]
+        if num_examples:
+            self.examples = self.examples[:num_examples]
         self.lang_code = os.path.basename(file).split('.')[0].split('_')[1]
         self.lang_token = c2t[self.lang_code]
         self.tokenizer = tokenizers[self.lang_token]
@@ -37,17 +41,17 @@ class ParallelDataset(Dataset):
     def __len__(self):
         return len(self.examples)
 
-def get_data_loader(file, batch_size, shuffle, num_workers, mono):
+def get_data_loader(file, batch_size, shuffle, num_workers, mono, num_examples=None):
     if mono:
         return DataLoader(
-            MonoDataset(file),
+            MonoDataset(file, num_examples=num_examples),
             batch_size=batch_size,
             shuffle=shuffle,
             num_workers=num_workers
         )
     else:
         return DataLoader(
-            ParallelDataset(file),
+            ParallelDataset(file, num_examples=num_examples),
             batch_size=batch_size,
             shuffle=shuffle,
             num_workers=num_workers
--- train.py
+++ train.py
@@ -7,26 +7,40 @@ from torch.cuda import empty_cache
 from torch import manual_seed
 from torch import optim
 
-from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoConfig, AutoModelForSeq2SeqLM
 
 from make_tokenizer import lang_code_to_lang_token as c2t
 from get_dataloaders import get_all
 from get_dataloader import get_data_loader
 
 def main():
-
+    
+    #device = 'cpu'
     device = 'cuda' if is_available() else 'cpu'
     manual_seed(42)
 
+    model_name = 'facebook/nllb-200-distilled-600M'
+    
+    config = AutoConfig.from_pretrained(model_name)
+    config.vocab_size += 8 # 8 new special tokens for languages
+    
     model = AutoModelForSeq2SeqLM.from_pretrained(
-        'facebook/nllb-200-distilled-600M'
+        model_name,
+        config=config,
+        ignore_mismatched_sizes=True
     )
+    
+    # for name, param in model.named_parameters():
+    #     print(name)
+    #     print(param.size())
+    #     print('\n')
+    # exit()
 
     model.to(device)
 
     """ HYPERPARAMETERS """
-    bs = 16
-    epochs = 3
+    bs = 1 # TODO: change to 16
+    epochs = 10
     lr = 1e-5
     eps = 1e-8
     betas = (0.9, 0.999)
@@ -55,22 +69,30 @@ def main():
     st_dataloaders = [
         get_data_loader(
             os.path.join('train', 'unprocessed_train', 'shared_task', 'es_ctp.train'),
-            batch_size=1,
+            batch_size=bs,
             shuffle=True,
             num_workers=4,
-            mono=False
+            mono=False,
+            num_examples=10
         )
     ]
 
-    """ TRAINING - MLM WITH MONOLINGUAL DATA """
+    """ TRAINING - PARALLEL DATA """
     for epoch in range(epochs):
         for dataloader in st_dataloaders:
 
             tokenizer = dataloader.dataset.tokenizer
             
+            assert len(dataloader) == 10
+            
             for i, batch in enumerate(dataloader):
                 
                 optimizer.zero_grad()
+                
+                # print(batch[0])
+                # print('\n\n\n')
+                # print(batch[1])
+                # print('\n\n\n')
                 
                 model_inputs = tokenizer(
                     text = batch[0],
@@ -80,6 +102,9 @@ def main():
                     truncation = True,
                     max_length = 1024
                 )
+                
+                # print(model_inputs)
+                # exit()
                 
                 # print('mask token id:', tokenizer.mask_token_id)
                 # print('mask token:', tokenizer.mask_token)
