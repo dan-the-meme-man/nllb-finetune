8f147d281e32eda5987be32c027245d379354ee4
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-03-14 02:50:31 -0400

    oh my god how do you decode

--- train.py
+++ train.py
@@ -127,18 +127,26 @@ def main():
     """ HYPERPARAMETERS """
     overfit = True # TODO: search for optimal hyperparameters
     num_workers       = 1
+    
     batch_size        = 8     if not overfit else 1
-    bad_epochs        = 1     if not overfit else 0
-    bad_num_batches   = 10000 if not overfit else 0
-    good_epochs       = 3     if not overfit else 0
-    good_num_batches  = 10000 if not overfit else 0
-    train_epochs      = 10    if not overfit else 30
-    train_num_batches = 10000 if not overfit else 5
-    dev_num_batches   = None  if not overfit else 0     # None for full dev set
     max_length        = 256   if not overfit else 64
     lang_code         = None  if not overfit else 'aym' # None for all languages
-    lr = 1e-5
-    weight_decay = 0.01
+    lr                = 1e-5
+    weight_decay      = 1e-2
+    
+    bad_epochs        = 1     if not overfit else 0
+    bad_num_batches   = 10000 if not overfit else 1
+    do_bad            = True  if not overfit else False
+    
+    good_epochs       = 3     if not overfit else 0
+    good_num_batches  = 10000 if not overfit else 1
+    do_good           = True  if not overfit else False
+    
+    train_epochs      = 10    if not overfit else 30
+    train_num_batches = 10000 if not overfit else 5
+    
+    dev_num_batches   = None  if not overfit else 1     # None for full dev set
+    do_dev            = True  if not overfit else False
     
     output_str = f'{batch_size}_{bad_epochs}_{bad_num_batches}_{good_epochs}'
     output_str += f'_{good_num_batches}_{train_epochs}_{train_num_batches}_{lr}_{weight_decay}'
@@ -153,58 +161,61 @@ def main():
         weight_decay=weight_decay
     )
     
-    print('Loading dev data...')
-    dev_loaders = get_data_loader(
-        split='dev',
-        batch_size=batch_size,
-        num_batches=dev_num_batches,
-        max_length=max_length,
-        lang_code=lang_code,
-        shuffle=False, # ignored
-        num_workers=num_workers,
-        use_tgts=True # for dev loss
-    )
-    print('Dev data loaded.\n')
+    if do_dev:
+        print('Loading dev data...')
+        dev_loaders = get_data_loader(
+            split='dev',
+            batch_size=batch_size,
+            num_batches=dev_num_batches,
+            max_length=max_length,
+            lang_code=lang_code,
+            shuffle=False, # ignored
+            num_workers=num_workers,
+            use_tgts=True # for dev loss
+        )
+        print('Dev data loaded.\n')
     
     """ TRAINING - BAD SUPP """
-    print('Training on bad supp...')
-    bad_train_losses, bad_dev_losses = train(
-        loader_name='bad_supp',
-        batch_size=batch_size,
-        num_batches=bad_num_batches,
-        max_length=max_length,
-        lang_code=lang_code,
-        num_workers=num_workers,
-        epochs=bad_epochs,
-        model=model,
-        optimizer=optimizer,
-        device=device,
-        dev_loaders=dev_loaders,
-        ckpt=False,
-        output_str=output_str,
-        do_dev=False,
-    )
-    print('Training on bad supp complete.\n')
+    if do_bad:
+        print('Training on bad supp...')
+        bad_train_losses, bad_dev_losses = train(
+            loader_name='bad_supp',
+            batch_size=batch_size,
+            num_batches=bad_num_batches,
+            max_length=max_length,
+            lang_code=lang_code,
+            num_workers=num_workers,
+            epochs=bad_epochs,
+            model=model,
+            optimizer=optimizer,
+            device=device,
+            dev_loaders=dev_loaders,
+            ckpt=False,
+            output_str=output_str,
+            do_dev=False,
+        )
+        print('Training on bad supp complete.\n')
     
     """ TRAINING - GOOD SUPP """
-    print('Training on good supp...')
-    good_train_losses, good_dev_losses = train(
-        loader_name='bad_supp',
-        batch_size=batch_size,
-        num_batches=good_num_batches,
-        max_length=max_length,
-        lang_code=lang_code,
-        num_workers=num_workers,
-        epochs=good_epochs,
-        model=model,
-        optimizer=optimizer,
-        device=device,
-        dev_loaders=dev_loaders,
-        ckpt=False,
-        output_str=output_str,
-        do_dev=False,
-    )
-    print('Training on good supp complete.\n')
+    if do_good:
+        print('Training on good supp...')
+        good_train_losses, good_dev_losses = train(
+            loader_name='bad_supp',
+            batch_size=batch_size,
+            num_batches=good_num_batches,
+            max_length=max_length,
+            lang_code=lang_code,
+            num_workers=num_workers,
+            epochs=good_epochs,
+            model=model,
+            optimizer=optimizer,
+            device=device,
+            dev_loaders=dev_loaders,
+            ckpt=False,
+            output_str=output_str,
+            do_dev=False,
+        )
+        print('Training on good supp complete.\n')
     
     """ TRAINING - TRAIN """
     print('Training on train...')
@@ -222,7 +233,7 @@ def main():
         dev_loaders=dev_loaders,
         ckpt=True,
         output_str=output_str,
-        do_dev=False
+        do_dev=do_dev
     )
     print('Training on train complete.\n')
     
