8fc00723759c9543e6cecb57e2a66826ae5d1e90
Dan DeGenaro <daniel.degenaro@gmail.com>
2024-03-20 12:16:38 -0400

    testing timing

--- train.py
+++ train.py
@@ -244,8 +244,8 @@ def main():
     """ HYPERPARAMETERS """ # TODO: search for optimal hyperparameters
     overfit           = True                             # overfit on small data to test functionality
     log_freq          = 100     if not overfit else 1     # frequency of logging in batches
-    num_workers       = 2                                 # number of workers for data loader
-    get_tokenized     = True                              # whether to get tokenized data
+    num_workers       = 1                                 # number of workers for data loader
+    get_tokenized     = False                              # whether to get tokenized data
     
     batch_size        = 16      if not overfit else 2     # batch size
     max_length        = 384     if not overfit else 16    # maximum length of input sequences
